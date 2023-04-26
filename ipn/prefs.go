c5a745c86991204a451a0b5ad31914320bf368c4
Chenyang Gao <gps949@outlook.com>
2023-04-26 21:45:04 +0800

    放开强制不使用官方服务器的限制
    Signed-off-by: Chenyang Gao <gps949@outlook.com>

--- ipn/prefs.go
+++ ipn/prefs.go
@@ -40,7 +40,7 @@ var (
 // IsLoginServerSynonym reports whether a URL is a drop-in replacement
 // for the primary Tailscale login server.
 func IsLoginServerSynonym(val any) bool {
-	return val == "https://login.tailscale.com" || val == "https://controlplane.tailscale.com"
+	return false // val == "https://login.tailscale.com" || val == "https://controlplane.tailscale.com"
 }
 
 // Prefs are the user modifiable settings of the Tailscale node agent.
